77f923f84b9662a8c15bc9a746db3ab67f1e1faa
MI_A_02\PaparellaA <alberto.paparella.88@gmail.com>
2018-01-23 12:59:36 +0100

    Proto Menù

---
+++ Assets/MenuController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class MenuController : MonoBehaviour
+{
+
+    public MenuController NextMenu;
+
+    public List<Button> Buttons = new List<Button>();
+
+    public Button ActualSelected
+    {
+        get { return Buttons[actualSelected]; }
+        set { Buttons[actualSelected] = value; }
+    }
+
+    int _actualSelected;
+    int actualSelected
+    {
+        get { return _actualSelected; }
+        set
+        {
+            if (value >= Buttons.Count)
+                _actualSelected = 0;
+            else
+                _actualSelected = value;
+        }
+    }
+
+
+    /// <summary>
+    /// Funzione da chiamare per andare al menù successi
+    /// </summary>
+    public void GoToNextMenu()
+    {
+
+    }
+
+    /// <summary>
+    /// Select the next menù item
+    /// </summary>
+    public void SelectNext()
+    {
+        actualSelected++;
+
+    }
+
+    /// <summary>
+    /// Select the next menù item
+    /// </summary>
+    public void SelectPrevious()
+    {
+        ActualSelected.Select();
+
+        actualSelected--;
+    }
+
+}
---
+++ Assets/tester.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tester : MonoBehaviour {
+
+    public MenuController MC;
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            MC.SelectNext();
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            MC.SelectPrevious();
+        }
+    }
+}
