907c03d313acf3c4bc69f2f495411c5b1ef59d72
MI_C_01\PaparellaA <alberto.paparella.88@gmail.com>
2018-01-25 14:55:48 +0100

    - Aggiunto Dotween -Sistemata la scena prototipo

---
+++ Assets/Proto/Script/Proto_CamZoom.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class Proto_CamZoom : MonoBehaviour {
+
+    public Material Unselected;
+    public Material Selected;
+
+    bool _isMO;
+    bool isMouseOver { get { return _isMO; }
+        set
+        {
+            if (_isMO == value)
+                return;
+
+            _isMO = value;
+            if (!isSelected)
+            {
+                if(isMouseOver)    
+                    mr.material = Selected;
+                else
+                    mr.material = Unselected;
+            }
+
+        }
+    }
+
+    bool _isSel;
+    bool isSelected {
+        get { return _isSel; }
+        set
+        {
+            if (_isSel == value)
+                return;
+
+            _isSel = value;
+            isMouseOver = false;
+            if(isSelected)
+                camCtrl.FocusAt(transform.position);
+            else
+                camCtrl.FocusReset();
+        }
+    }
+
+    MeshRenderer mr;
+    CameraControl camCtrl;
+
+    private void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+        camCtrl = Camera.main.GetComponent<CameraControl>();
+    }
+
+    private void OnMouseEnter()
+    {
+        isMouseOver = true;
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0) && isMouseOver)
+            isSelected = true;
+        if (Input.GetMouseButtonDown(1) && !isMouseOver)
+            isSelected = false;
+    }
+
+    private void OnMouseExit()
+    {
+        isMouseOver = false;
+    }
+}
