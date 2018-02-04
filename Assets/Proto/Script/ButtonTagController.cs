cfed2373541f1f204b9c55df94c637d773a419ee
AlbertoDesktop <alberto.paparella.88@gmail.com>
2018-02-04 01:20:52 +0100

    parziale struttura puzzle1

---
+++ Assets/Proto/Script/ButtonTagController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ButtonTagController : MonoBehaviour {
+
+    public TextMesh Label;
+    public GameObject ButtonGraphic;
+
+    private void Start()
+    {
+        Label = GetComponentInChildren<TextMesh>();
+    }
+
+    public void OverrideLabel(string _text)
+    {
+        Label.text = _text;
+    }
+}
---
+++ Assets/Proto/Script/SliderController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderController : MonoBehaviour {
+
+    public float SmallerValue = -0.085f;
+    public float HigherValue = 0.085f;
+    float maxLenght { get { return HigherValue - SmallerValue; } }
+
+    public Material PositiveColor;
+    public Material NeutralColor;
+    public Material NegativeColor;
+
+    LineRenderer renderer;
+
+    private void Start()
+    {
+        renderer = GetComponentInChildren<LineRenderer>();
+    }
+
+    public void SetFillAmount(float _percentage)
+    {
+        Vector3 newLineHead = renderer.GetPosition(renderer.positionCount - 1);
+
+        newLineHead.y = SmallerValue + maxLenght * _percentage / 100;
+
+        renderer.SetPosition(renderer.positionCount - 1, newLineHead);
+
+        AdaptMaterial(_percentage);
+    }
+
+    void AdaptMaterial(float _percentage)
+    {
+        if (_percentage > 80)
+            renderer.material = NegativeColor;
+        else if (_percentage < 70)
+            renderer.material = NeutralColor;
+        else
+            renderer.material = PositiveColor;
+    }
+}
