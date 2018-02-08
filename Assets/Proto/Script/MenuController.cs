d9e845d95385a5c272271d4427f33388be9ac57e
AlbertoScuola_Alpha <alberto.paparella.88@gmail.com>
2018-02-08 11:00:15 +0100

    Build v.0.3.1

---
+++ Assets/Proto/Script/MenuController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject MainMenuPanel;
+    public GameObject LevelSelectionPanel;
+    EventSystem currentES;
+
+    private void Start()
+    {
+        currentES = FindObjectOfType<EventSystem>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+            GoToMainMenu();
+    }
+
+    #region API
+    public void GoToMainMenu()
+    {
+        if (!MainMenuPanel.activeSelf)
+        {
+            MainMenuPanel.SetActive(true);
+            ResetEventSystmSelection(MainMenuPanel.GetComponentInChildren<Button>().gameObject);
+        }
+
+        if (LevelSelectionPanel.activeSelf)
+            LevelSelectionPanel.SetActive(false);
+    }
+
+    public void GoToSelectionMenu()
+    {
+        if (MainMenuPanel.activeSelf)
+            MainMenuPanel.SetActive(false);
+
+        if (!LevelSelectionPanel.activeSelf)
+        {
+            LevelSelectionPanel.SetActive(true);
+            ResetEventSystmSelection(LevelSelectionPanel.GetComponentInChildren<Button>().gameObject);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void GoGamePlay(int _sceneIndex = 1)
+    {
+        SceneManager.LoadScene(_sceneIndex);
+    }
+    #endregion
+
+    void ResetEventSystmSelection(GameObject _selection)
+    {
+        currentES.SetSelectedGameObject(_selection);
+    }
+}
---
+++ Assets/Proto/Script/MenuPauseController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPauseController : MonoBehaviour
+{
+    public GameObject PausePanel;
+    EventSystem currentES;
+
+    private void Start()
+    {
+        currentES = FindObjectOfType<EventSystem>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+            TogglePauseMenu();
+    }
+
+
+    public void GoMainMenu(int _sceneIndex = 0)
+    {
+        SceneManager.LoadScene(_sceneIndex);
+    }
+
+    public void TogglePauseMenu()
+    {
+        if (PausePanel.activeSelf)
+            PausePanel.SetActive(false);
+        else
+        {
+            PausePanel.SetActive(true);
+            currentES.SetSelectedGameObject(PausePanel.GetComponentInChildren<Button>().gameObject);
+        }
+    }
+}
