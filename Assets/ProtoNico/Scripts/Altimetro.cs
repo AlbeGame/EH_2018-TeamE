d068b61b15c0d9d351b148bbecfc8c6be2ae102d
Nico925 <oloccin925@hotmail.it>
2018-02-12 05:08:53 +0100

    da modificare ancora

--- Assets/ProtoNico/Scripts/Altimetro.cs
+++ Assets/ProtoNico/Scripts/Altimetro.cs
@@ -5,23 +5,21 @@ using UnityEngine;
 public class Altimetro : MonoBehaviour {
 
 
-    public GameObject altimetro;
+    /*public GameObject altimetro;
 
     public float angleMin = 1;
     public float angleMax = 10;
     public float actualAltitude;
     public float altitudeHigh = 20;
     public float altitudeLow = 2;
-
+    */
 
 
 
 
     private void Update()
     {
-        transform.eulerAngles = new Vector3(angleMin,angleMax,Time.deltaTime);
-        float needleAngle = Mathf.Lerp(angleMin, angleMax, actualAltitude / (altitudeHigh - altitudeLow));
-
+       
     }
 
 
