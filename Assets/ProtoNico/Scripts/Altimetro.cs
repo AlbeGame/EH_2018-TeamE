fff8d828b4f7f1b87bd6c95ecdce6768441d5fad
Nico925 <oloccin925@hotmail.it>
2018-02-15 04:05:06 +0100

    sistemato l'altimetro con timer e aggiunte le seguenti API, ho lasciato il gameover.text.. mentre ho tolto il timer.text.

--- Assets/ProtoNico/Scripts/Altimetro.cs
+++ Assets/ProtoNico/Scripts/Altimetro.cs
@@ -1,29 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
-public class Altimetro : MonoBehaviour {
+public class Altimetro : MonoBehaviour
+{
 
-     const float degreeForSecond = 10f;
-     public Transform freccia_Altimetro;
+    const float degreeForSecond = 10f;
 
-    private float time = 1;
+    public Transform freccia_Altimetro;
+    public Text gameOverText;
 
-  
-     private void Update()
-     {
-         MoveFreccia();
-     }
+    private float timePos = -40.0f;
+    private float timer = 35;
 
-
-     public void MoveFreccia()
-     {
-         time += Time.deltaTime;
-         freccia_Altimetro.localRotation = Quaternion.Euler(0, 0, time * degreeForSecond);
-
-     }
+    void Start()
+    {
+        gameOverText.text = "";
+    }
 
 
+    private void Update()
+    {
+        GetCurrentTime();
+        DecreaseTime();
+
+    }
+
+
+    public void GetCurrentTime()
+    {
+        timePos += Time.deltaTime;
+        freccia_Altimetro.localRotation = Quaternion.Euler(0, 0, timePos * degreeForSecond);
+
+    }
+
+    public void DecreaseTime()
+    {
+        timer -= Time.deltaTime;
+        {
+
+
+            if (timer <= 0f)
+            {
+                gameOverText.text = "GAMER OVER";
+                Time.timeScale = 0;
+
+            }
+
+
+
+        }
+    }
 
 }
