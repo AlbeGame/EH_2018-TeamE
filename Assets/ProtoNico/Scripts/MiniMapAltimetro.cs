2f518f823684f57a627e1dc1a82bc858a84aa202
Nico925 <oloccin925@hotmail.it>
2018-03-05 02:48:16 +0100

    aggiunto mini mappa altimetro

---
+++ Assets/ProtoNico/Scripts/MiniMapAltimetro.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MiniMapAltimetro : MonoBehaviour {
+
+    public GameObject mapAltimeter;
+    bool isActiveMap;
+
+
+
+
+	void Start () {
+        NotActive();
+	}
+	
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (isActiveMap)
+            {
+                NotActive();
+            }
+
+            else
+            {
+                ActiveMap();
+            }
+        }
+        
+      
+    }
+
+
+    void ActiveMap()
+    {
+        isActiveMap = true;
+        mapAltimeter.SetActive(true);
+    }
+
+    void NotActive()
+    {
+        isActiveMap = false;
+        mapAltimeter.SetActive(false);
+    }
+
+}
