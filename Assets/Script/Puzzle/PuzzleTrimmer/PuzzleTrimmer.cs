328347b91d10669df0f47ee4debb68c9133e6cf1
Nico925 <oloccin925@hotmail.it>
2018-05-30 05:08:29 +0200

    implementato le interfacce ..ancora da finire

---
+++ Assets/Script/Puzzle/PuzzleTrimmer/PuzzleTrimmer.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTrimmer : MonoBehaviour, IPuzzle,ISelectable {
+
+
+    public PuzzleState SolutionState
+    {
+        get
+        {
+            throw new System.NotImplementedException();
+        }
+
+        set
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public bool CheckIfSolved()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DoLoose()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DoWin()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Init()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnButtonSelect(SelectableButton _button)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnMonitorSelect(SelectableMonitor _monitor)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSelection()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnStateChange(SelectionState _state)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSwitchSelect(SelectableSwitch _switch)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUpdateSelectable(IPuzzleInput _input)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Setup(IPuzzleData data)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/Puzzle/PuzzleTrimmer/PuzzleTrimmerData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTrimmerData :ScriptableObject,IPuzzleData {
+
+
+    public GameObject GetIPuzzleGO()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+}
